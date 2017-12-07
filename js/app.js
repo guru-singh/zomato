4cc2de8d6850be011be81ee259d37ef33b3d4791
guru sing <singh.guru@gmail.com>
2017-12-07 23:48:10 +0530

    project started

---
+++ js/app.js
@@ -0,0 +1,93 @@
+/**
+ * [APIKey description]
+ * @type {String}
+ */
+ APIKey = 'e6773c46c9ff362ed63679ca11393f20';
+ Mumbai = 3;
+ NewYork = 0;
+ Nottingham = 0;
+
+/**
+ * @param  {[type]}
+ * @return {[type]}
+ */
+function globalAjax(API){
+
+	let url = 'https://developers.zomato.com/api/v2.1/'+API+"?q=NewYork&count=20"
+	$.ajax({
+  		type: "GET",
+  		headers: {
+        	'user-key': APIKey,
+        	'Content-Type':'application/json'
+    	},
+  		url: url,
+  		data: "json=somedata",
+  		processData: false,
+  		success: function(msg) {
+  			parseResponse(API, msg);
+  		},
+  		failure:function(msg) {
+			alert('failure')
+  		}
+	});
+}
+
+
+$( document ).ready(function() {
+    listCities();
+});
+
+
+function listCities(){
+	let me = this;
+	globalAjax('cities');
+	
+
+}
+/**
+ * [parseResponse description]
+ * @param  {[type]} API         [description]
+ * @param  {[type]} responseMsg [description]
+ * @return {[type]}             [description]
+ */
+function parseResponse(API, responseMsg){
+	let response = responseMsg,
+		locations  =response.location_suggestions
+	;
+	switch(API){
+		case 'cities':
+			//console.log('call list cities');
+			listCitiesToPage(locations);
+		break;
+		default:
+			console.log('call default');
+		break;
+	}
+	if(response.status === 'success') {
+		$.each(locations, function(i, location) {
+    		//console.log(location.name);
+    		
+		})
+	}
+
+}
+
+
+function listCitiesToPage(lists)
+{
+	let ul = $('#cityList');
+
+	$.each(lists,function(i,list) {
+		//a = $('<a>', {href: '/'+list.name}).appendTo(ul);
+		let a = $('<a >', {onClick: 'listCuisines('+ list.id +', cityList)'}).appendTo(ul);
+		$('<li>',{text:list.name, class:"text"}).appendTo(a);
+		
+    	//
+	});
+}
+
+
+function listCuisines(cityID, prevEl) {
+	$('#cityList').hide("slide");
+}
+
